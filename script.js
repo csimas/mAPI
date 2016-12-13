816d072fdae21aaa4619f9a6f3db5baddfec2028
Jackie Lin <jackielin13@gmail.com>
2016-12-12 22:48:13 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+
+//adapted from https://developers.google.com/maps/documentation/javascript/examples/place-radar-search
+var map;
+var infoWindow;
+var service;
+var latitude=40.730
+var longitude=-74.006
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('right'), {
+		  center: {lat: latitude, lng: longitude},
+		  zoom: 13,
+		  styles: [{
+			    stylers: [{ visibility: 'simplified' }]
+		  }, {
+			    elementType: 'labels',
+			    stylers: [{ visibility: 'off' }]
+		  }]
+	});
+	infoWindow = new google.maps.InfoWindow();
+	service = new google.maps.places.PlacesService(map);
+	map.addListener('idle', performSearch);
+}
+
+function performSearch() {
+	var request = {
+		  bounds: map.getBounds(),
+		  keyword: 'catcall'
+	};
+	service.radarSearch(request, callback);
+}
+var actuals;
+
+function callback(results, status) {
+	if (status !== google.maps.places.PlacesServiceStatus.OK) {
+	  console.error(status);
+	  return;
+	}
+	for (var i = 0, result; result = results[i]; i++) {
+
+
+	 // 	var service = new google.maps.places.PlacesService(map);
+
+        service.getDetails({
+          placeId: result.place_id
+        }, function(place, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+              	if(place.reviews != null){  //checks to see if place has review
+              		document.getElementById('results').innerHTML+=
+		              		'<div><strong>' + place.name + '</strong><br>' + 'Place ID: ' + place.place_id + '<br>' + place.formatted_address + '</div>'; //dumby text
+	              	for(var j =0,review;review=place.reviews[j];j++){
+	              		if(review.text.search("catcall")>-1){ //checks for strings (here just checking for placeholder searchstr 'rooms')
+	              			document.getElementById('results').innerHTML+=
+		              		'<div>' + place.reviews[j].text + '</div>'; //dumby text
+	              		}
+	              	}
+	            }	
+		              	
+          }
+          
+        });
+
+
+        //
+	  	addMarker(result);
+	}
+}
+
+function addMarker(place) {
+	var marker = new google.maps.Marker({
+	  map: map,
+	  position: place.geometry.location,
+	  icon: {
+	    url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
+	    anchor: new google.maps.Point(10, 10),
+	    scaledSize: new google.maps.Size(10, 17)
+	  }
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+	  service.getDetails(place, function(result, status) {
+	    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+	      console.error(status);
+	      return;
+	    }
+	    infoWindow.setContent(result.name);
+	    infoWindow.open(map, marker);
+	  });
+	});
+}
