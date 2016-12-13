1e765164bdbb5ec71ac21af15d866e6dd95676d9
Jackie Lin <jackielin13@gmail.com>
2016-12-13 00:50:31 -0500

    added review search functions and a stemmer.js

--- script.js
+++ script.js
@@ -1,3 +1,194 @@
+var dictionary_json = {
+	"race": [
+		"aryan",
+		"apu",
+		"beaner",
+		"bigot",
+		"brownie",
+		"camel",
+		"charlie",
+		"chinaman",
+		"ching chong",
+		"chink",
+		"cholo",
+		"colored",
+		"coon",
+		"cracker",
+		"darky",
+		"darkie",
+		"dothead",
+		"dot head",
+		"dot-head",
+		"featherhead",
+		"ghetto",
+		"gook",
+		"greaseball",
+		"greaser",
+		"gringo",
+		"guido",
+		"guinea",
+		"gypsy",
+		"halfbreed",
+		"half breed",
+		"honky",
+		"injun",
+		"jap",
+		"jigaboo",
+		"jigga",
+		"lynch",
+		"klan",
+		"kkk",
+		"macaca",
+		"mongrel",
+		"mulatto",
+		"mutt",
+		"nazi",
+		"negr",
+		"nigger",
+		"nigga",
+		"niggah",
+		"niggress",
+		"orient",
+		"oriental",
+		"racial",
+		"racism",
+		"racist",
+		"redskin",
+		"spic",
+		"spick",
+		"spig",
+		"spigotty",
+		"tacohead",
+		"uncle tom",
+		"welfare",
+		"white trash"
+	],
+
+	"gender": [
+		"bitch",
+		"breastfeed",
+		"catcall",
+		"grope",
+		"mansplain",
+		"misogyn",
+		"sexism",
+		"sexist",
+		"slut"
+	],
+
+	"LGBTQ": [
+		"butch",
+		"carpet muncher",
+		"dyke",
+		"gay",
+		"homo",
+		"lesbian",
+		"lesbo",
+		"dragqueen",
+		"fag",
+		"fairy",
+		"flamer",
+		"tranny"
+	],
+		
+	"religion": [
+		"goy",
+		"hijab",
+		"hindu",
+		"islam",
+		"jew",
+		"jihad",
+		"kike",
+		"muslim",
+		"osama",
+		"shylock",
+		"terrorist",
+		"towelhead",
+		"towel head",
+		"raghead",
+		"rag head"
+	],
+
+	"disability":[
+		"blind",
+		"deaf",
+		"dwarf",
+		"handicapped",
+		"midget",
+		"mongol",
+		"mongoloid",
+		"retard",
+		"retarded",
+		"tard",
+		"wheelchair"
+	],
+
+	"immigration status" :[
+		"anchor baby",
+		"durka",
+		"eurotrash",
+		"fresh off the boat",
+		"fob",
+		"get out",
+		"illegals",
+		"immigra",
+		"speak english",
+		"no english",
+		"mick",
+		"our country",
+		"paki",
+		"polack",
+		"wetback"
+	],
+
+	"violence": [
+		"attack",
+		"hit",
+		"punch",
+		"kick",
+		"grab",
+		"throw"
+	],
+
+	"hate speech": [
+		"discrimin",
+		"harass",
+		"prejudice",
+		"hate crime"
+	]
+};
+
+function init() {
+  dictionaryInit();
+}
+document.addEventListener("DOMContentLoaded", init, false);
+
+function dictionaryInit() {
+	document.dictionary = dictionary_json;
+	console.log(document.dictionary["race"]);
+	// for (var index in document.dictionary["race"]){
+	// 	console.log(document.dictionary["race"][index])
+	// }
+
+	// console.log($.inArray("camel", document.dictionary["race"]));
+
+	// console.log(stemmer("prejudice"));
+
+	if(containsHate("Everyone needs to stop using the word 'Oriental'.", "race")) {
+		console.log("test true");
+	}
+}
+
+function containsHate(review, filter) {
+	var review_arr = review.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g, "").toLowerCase().split(" ");
+	console.log(review_arr);
+	for (var i in document.dictionary[filter]){
+		if(review_arr.indexOf(document.dictionary[filter][i]) > -1) {
+			return true;
+		}
+	}
+	return false;
+}
 
 //adapted from https://developers.google.com/maps/documentation/javascript/examples/place-radar-search
 var map;
